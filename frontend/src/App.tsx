6e371721a3518a5e68755df0e5b55431765cfdf9
carlinhosalma <carlinhosalma@gmail.com>
2021-11-16 12:47:34 -0200

    primeiro commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+         O maior banco de projetos de sustentabilidade do Brasil
+        </p>
+        
+          <b>BANCO VERDE</b>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
